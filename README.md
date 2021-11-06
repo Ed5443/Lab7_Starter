01a07de596b8ea88f72f83ec889e8f0a76e88f5a
Ed5443 <efchang@ucsd.edu>
2021-11-06 15:24:58 -0700

    Test part 2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 7 - Starter Code
+
+https://ed5443.github.io/Lab7_Starter/
--- sw.js
+++ sw.js
@@ -9,6 +9,19 @@ self.addEventListener('install', function (event) {
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache){
+      console.log('Opened Cache');
+      return cache.addAll(['/','assets/scripts/main.js','assets/styles/main.css', 'assets/scripts/router.js',
+      'assets/components/RecipeCard.js', 'assets/components/RecipeExpand.js','https://introweb.tech/assets/json/ghostCookies.json',
+      'https://introweb.tech/assets/json/birthdayCake.json',
+      'https://introweb.tech/assets/json/chocolateChip.json',
+      'https://introweb.tech/assets/json/stuffing.json',
+      'https://introweb.tech/assets/json/turkey.json',
+      'https://introweb.tech/assets/json/pumpkinPie.json']);
+    })
+  );
 });
 
 /**
@@ -21,6 +34,17 @@ self.addEventListener('activate', function (event) {
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+  event.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(
+        cacheNames.map(function(cacheName){
+          if(cacheName != CACHE_NAME){
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 // Intercept fetch requests and store them in the cache
@@ -29,4 +53,26 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if(response){
+        return response;
+      }
+      return fetch(event.request).then(
+        function(response) {
+          if(!response || response.status !== 200 || response.type !== 'basic'){
+            return response;
+          }
+
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+          .then(function(cache){
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        }
+      );
+    })
+  );
 });
